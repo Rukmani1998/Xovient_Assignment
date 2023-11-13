abc5c9adf7f5e1999adfdef66807565e13f83371
krgup <krgup@LENOVO>
2023-11-14 01:13:19 +0530

    Add project files.

---
+++ Q1-RestAPI/RestAPIApp/RestAPIApp/Controllers/UsersController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+using RestAPIApp.Models;
+using System.Net.Http.Json;
+using Microsoft.Extensions.Configuration;
+using System.Net;
+
+namespace RestAPIApp.Controllers
+{
+    [Route("api/Users")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private IConfiguration configuration;        
+        private readonly HttpClient _client; 
+        public UsersController(IConfiguration iConfig)
+        {
+            _client = new HttpClient();
+            configuration = iConfig;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Users>> GetUsers()
+        {
+            try
+            {
+                Uri JsonUrl = new Uri(configuration.GetSection("MySettings").GetSection("JsonURL").Value);
+                _client.BaseAddress = JsonUrl;
+                var users = new List<Users>();
+
+                HttpResponseMessage response = _client.GetAsync(JsonUrl).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string jsonString = response.Content.ReadAsStringAsync().Result;
+                    users = JsonConvert.DeserializeObject<List<Users>>(jsonString);
+                }
+
+                return users;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ Q2-FindMissingElementsInArray/FindMissingArrayList/FindMissingArrayList/Program.cs
@@ -0,0 +1,98 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System;
+using System.Collections;
+using System.Globalization;
+using System.Xml.Linq;
+
+namespace SortArrayExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int[] intArray = new int[] { 2, 9, 4, 3, 5, 1, 7, 20};
+                printArrayList(intArray, "Current array elements are: ");
+                
+                int[] SortedArray = SortArrayInAccendingOrder(intArray);
+                printMulMissElmtFrmSortedArr(SortedArray, SortedArray.Length);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// sorts the unorderd array
+        /// </summary>
+        /// <param name="intArray"></param>
+        /// <returns></returns>
+        private static int[] SortArrayInAccendingOrder(int[] intArray)
+        {
+            int temp = 0;
+            for (int i = 0; i <= intArray.Length - 1; i++)
+            {
+                for (int j = i + 1; j < intArray.Length; j++)
+                {
+                    if (intArray[i] > intArray[j])
+                    {
+                        temp = intArray[i];
+                        intArray[i] = intArray[j];
+                        intArray[j] = temp;
+                    }
+                }
+            }
+
+            printArrayList(intArray, "Asscending order sorted array: ");         
+            
+            return intArray;
+        }
+
+        /// <summary>
+        /// Prints missing elemets from array
+        /// </summary>
+        /// <param name="intArray"></param>
+        /// <param name="N"></param>
+        private static void printMulMissElmtFrmSortedArr(int[] intArray, int N)
+        {
+            int diff =intArray[0] - 0;
+            string str="";
+            for (int i = 0; i < N; i++)
+            {
+                if (intArray[i] - i != diff)
+                {
+                    while (diff < intArray[i] - i)
+                    {
+                        //Console.Write(i + diff + ",");
+                        if (str == "") str = (i + diff).ToString();
+                        else str = str + "," + (i + diff);
+                        diff++;
+                    }
+                }
+            }
+
+            Console.WriteLine("Missing elements in array are: " + str);
+        }
+
+        /// <summary>
+        /// Display the array elements coma separated
+        /// </summary>
+        /// <param name="intArray"></param>
+        /// <param name="msg"></param>
+        static void printArrayList(int[] intArray, string msg)
+        {
+            string str = "";
+            foreach (var item in intArray)
+            {
+                if (str == "") str = item.ToString();
+                else str = str + "," + item;
+            }
+            Console.WriteLine(msg + str);
+        }
+
+
+    }
+}
